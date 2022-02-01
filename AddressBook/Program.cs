76df542f5bd002c5b4bf8a14b65b814e9fbd5d0d
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-01 20:38:20 +0530

    Adding Address Book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program\n");
+
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContactDetails("Guruprasad", "Kumbar", "Kothali", "Chikodi", "Karnataka", 591287, 9113544214, "guruprasadk.dev@gmail.com");
+
+            Console.WriteLine("Please choose an option from the below list");
+            Console.WriteLine("\n1. Add New Contact \n2. View Contact \n3. Edit Contact \n4. Delete Contact \n5. Exit\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addressBook.AddNewContact();
+                    break;
+                case 2:
+                    addressBook.ViewContact();
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the First Name to Edit Contact Details: ");
+                    string input = Console.ReadLine();
+                    addressBook.EditContact(input);
+                    addressBook.ViewContact();
+                    break;
+                case 4:
+                    Console.WriteLine("Enter the First Name of Contact: ");
+                    string fName = Console.ReadLine();
+                    Console.WriteLine("Enter the Last Name to Delete Contact: ");
+                    string lName = Console.ReadLine();
+                    addressBook.DeleteContact(fName, lName);
+                    addressBook.ViewContact();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Please choose the correct option");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
